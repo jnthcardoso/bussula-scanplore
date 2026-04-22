7e14b2ba02498585184d2f486be0cec901b4e17b
jnthcardoso <vagasjonathancardoso@gmail.com>
2026-04-22 16:00:34 -0300

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://cyqwqokuprdotqmizyuf.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5cXdxb2t1cHJkb3RxbWl6eXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NjAzNDksImV4cCI6MjA5MjQzNjM0OX0.CCLXQeM2-TYNRY3_UjTrLS-h_sEj5BipQ5knGHhf3Y0'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
